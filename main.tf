ce36be2432da93892cc1607b73afa0c13502635e
peterskipper <peter.skipper@gmail.com>
2020-09-03 15:23:31 -0500

    Add terraform (#2)
    * with module
    
    * added edits to make docker run command work
    
    * in progress
    
    * working AMI manually and updated run cmd (duh)
    
    * finally working
    
    * housekeeping

---
+++ main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_default_vpc" "default_vpc" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_security_group" "sec_grp" {
+  name   = "dashboard_sec_grp"
+  vpc_id = aws_default_vpc.default_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+resource "aws_instance" "dashboard" {
+  # Amazon Linux AMI amzn-ami-2018.03.20200805 x86_64 ECS HVM GP2 
+  ami           = "ami-0e18fc717d49b88a1"
+  instance_type = "t2.micro"
+  key_name      = "aws-pskip-2020-08-10"
+
+  vpc_security_group_ids = [aws_security_group.sec_grp.id]
+  tags = {
+    Name = "dashboard_cd"
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    docker pull peterskipper/dashboard_cd
+    docker run -p 80:8501 peterskipper/dashboard_cd
+    EOF
+}
+
+output "dashboard_ip" {
+  value       = aws_instance.dashboard.public_ip
+  description = "The public IP of the Dashboard"
+}
+
+output "dashboard_public_dns" {
+  value       = aws_instance.dashboard.public_dns
+  description = "Public DNS for your default VPC"
+}
